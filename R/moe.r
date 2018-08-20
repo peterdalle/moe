c2d7c9f1602cda02c323a06c174c6cb4077a6ebf
peterdalle <peterdalle@gmail.com>
2018-08-20 20:49:14 +0200

    Version 0.9.1 with S3 support

--- R/moe.r
+++ R/moe.r
@@ -1,47 +1,65 @@
 #' Calculate margin of error for simple probability samples
 #'
 #' \code{moe} calculates margin of error and confidence intervals for simple probability
-#' samples, as well as results formatted in accordance with American Psychological Association standards (APA6) and
-#' human-readable interpretations of the results.
+#' samples, as well as results formatted in accordance with American Psychological Association standards (APA6).
 #'
 #' @param proportion value between 0 and 1 indicating the proportion, such as 0.30 for 30 percent.
 #' @param n sample size.
 #' @param conf.level confidence level (defaults to \code{0.95}).
 #' @param digits number of decimal digits used when formatting the results as APA and human-readable messages (defaults to \code{2}).
-#' @param population.correction whether or not results should be corrected by population size (defaults to \code{FALSE}).
+#' @param population.correction whether or not results should be corrected by population size (defaults to \code{FALSE}), using the Finite Population Correction technique.
 #' @param population.size population size used by the population correction (defaults to \code{NULL}). Only used if \code{population.correction} is set to \code{TRUE}.
-#' @return a list with margin of error, confidence level, confidence interval (lower and upper bound), z-value,
-#' results formatted in accordance with APA6 standards, and human-readable interpretation of results.
+#' @return a list with \code{margin.of.error} (margin of error), \code{conf.level} (confidence level), \code{conf.lower} (confidence interval lower bound), \code{conf.upper} (confidence interval upper bound), \code{proportion} (proportion), \code{percentage} (percentage), \code{z.value} (z-value from normal distribution), \code{digits} (number of digits used to format APA confidence intervals), \code{n} (sample size), \code{population.corrected} (whether or not the margin of error is corrected for population size), \code{population.size} (population size), \code{fpc} (finite population correction, between 0 and 1), \code{sampling.fraction} (sampling fraction, ratio of sample size to population size, between 0 and 1), \code{error.uncorrected} (margin of error before it is corrected for population size), and \code{apa} (APA6 style formatted confidence intervals).
 #' @section Assumptions:
-#' \code{moe} assumes a normal distribution by calculating the z-value from \code{qnorm}.
+#' \code{moe} assumes a normal distribution by calculating the z-value from \code{qnorm}, as well as simple random sampling (i.e., all observations have an equal probability of inclusion).
+#' @section Finite population correction:
+#' When the sampling fraction (ratio of sample size to population size) is large, approximately 5 percent or more, the estimate of the standard error can be corrected by multiplying a \href{https://en.wikipedia.org/wiki/Standard_error#Correction_for_finite_population}{Finite Population Correction}. To use this correction, set the \code{population.correction} argument to \code{TRUE} and set the sample size for the population using the \code{population.size} argument.
+#' @section References:
+#' Bondy, W. & Zlot, W. (1976). The Standard Error of the Mean and the Difference Between Means for Finite Populations. The American Statistician, 30, 96–97. doi:10.2307/2683803
 #' @seealso \link[stats]{qnorm()}
 #' @examples
 #' \dontrun{
-#' # Margin of error for party with 30% of voters in a sample of 1,200.
+#' # Margin of error for party with 30%
+#' # of voters in a sample of 1,200.
 #' moe(proportion=0.30, n=1200)
 #'
-#' # Margin of error for party with 30% of voters in a sample of 50,000,
-#' # corrected for population size (300,000) and 99% confidence level.
-#' moe(proportion=0.30, n=50000, conf.level=0.99,
+#' # Correct for population size (N=300,000),
+#' # using 99% confidence level.
+#' moe(proportion=0.30, n=1200, conf.level=0.99,
 #'     population.correction=TRUE, population.size=300000)
 #'
-#' # Extract confidence interval.
+#' # Get confidence interval.
 #' m <- moe(proportion=0.30, n=1200)
 #' m$conf.lower
 #' m$conf.upper
 #'
-#' # Extract confidence interval in APA6 format.
-#' m$apa
+#' # Show all information.
+#' summary(m, digits=2)
+#'
+#' # APA6 style confidence intervals.
+#' as.character(m, digits=2)
+#'
+#' # Print margin of error.
+#' print(m, digits=2)
+#'
+#' # 2-sample test for equality of proportions
+#' # (Chi-square) using the minus operator.
+#' m1 <- moe(proportion=0.33, n=1200)
+#' m2 <- moe(proportion=0.40, n=1200)
+#' m1 - m2
 #' }
 #' @export
 moe <- function(proportion, n, conf.level=.95, digits=2, population.correction=FALSE, population.size=NULL) {
   # Failchecks.
   if(proportion < 0 | proportion > 1) { stop("proportion must be a value beteen 0 and 1, such as 0.30 for 30%.") }
   if(conf.level < 0 | conf.level > 1) { stop("conf.level must be a value beteen 0 and 1, such as 0.95 for 95%.") }
-  if(population.correction) { if(is.null(population.size)) { stop("If population.correction is TRUE, a population.size must be set.") } }
-  if(!is.null(population.size)) { if(population.size < 0) { stop("population.size cannot be negative.") } }
-  if(digits < 0) { stop("digits cannot be negative.") }
   if(n < 0) { stop("n cannot be negative.") }
+  if(digits < 0) { stop("digits cannot be negative.") }
+  if(population.correction) {
+    if(is.null(population.size)) { stop("If population.correction is TRUE, a population.size must be set.") }
+    if(population.size < 0) { stop("population.size cannot be negative.") }
+    if(population.size < n) { stop("population.size cannot be smaller than sample size n.") }
+  }
 
   # Convert confidence level to two-sided alpha and its z-value.
   alpha <- (1 - conf.level) / 2
@@ -50,38 +68,170 @@ moe <- function(proportion, n, conf.level=.95, digits=2, population.correction=F
   # Calculate margin of error.
   error <- z * (sqrt(proportion * (1 - proportion) / n)) * 100
 
-  # Use population correction for margin of error?
+  sampling.fraction <- NULL
+  if(!is.null(population.size)) {
+    # Sampling fraction (ratio of sample size to population size).
+    sampling.fraction <- n / population.size
+  }
+
+  # Use Finite Population Correction (FPC) for margin of error?
+  fpc <- NULL
+  error.uncorrected <- NULL
   if(population.correction) {
-    error <- error * (1 - (n / population.size))
+    fpc <- (1 - (n / population.size))
+    error.uncorrected <- error
+    error <- error * fpc
   }
 
   # Calculate confidence lower and upper bounds.
   conf.lower <- proportion * 100 - error
   conf.upper <- proportion * 100 + error
 
-  return(list(margin.of.error = error,                       # Margin of error (in percentage points).
-              conf.level = round(conf.level * 100, 0),       # Confidence level.
-              conf.lower = conf.lower,                       # Confidence interval lower bound.
-              conf.upper = conf.upper,                       # Confidence interval upper bound.
-              proportion = proportion,                       # Proportion.
-              percentage = round(proportion * 100, digits),  # Proportion expressed as a percentage.
-              z.value = z,                                   # Z-value from confidence level.
-              digits = digits,                               # Number of digits when formatting the APA and human-readable results.
-              n = n,                                         # Sample size.
-              population.corrected = population.correction,  # Whether or not population correction method was applied.
-              population.size = population.size,             # The population size used in the population correction method.
+  # Save parameters to list.
+  moeobj <- list(margin.of.error = error,                        # Margin of error (in percentage points).
+                  conf.level = round(conf.level * 100, 0),       # Confidence level (in percentage points).
+                  conf.lower = conf.lower,                       # Confidence interval lower bound (in percentage points).
+                  conf.upper = conf.upper,                       # Confidence interval upper bound (in percentage points).
+                  proportion = proportion,                       # Proportion.
+                  percentage = proportion * 100,                 # Proportion expressed as a percentage.
+                  z.value = z,                                   # Z-value from confidence level.
+                  digits = digits,                               # Number of digits when formatting the APA and human-readable results.
+                  n = n,                                         # Sample size.
+                  population.corrected = population.correction,  # Whether or not population correction method was applied.
+                  population.size = population.size,             # The population size used in the population correction method (only used when population.corrected is TRUE).
+                  fpc = fpc,                                     # Finite population correction (only used when population.corrected is TRUE).
+                  sampling.fraction = sampling.fraction,         # Sampling fraction (only used when population.corrected is TRUE).
+                  error.uncorrected = error.uncorrected,         # Margin of error without finite population correction (only used when population.corrected is TRUE).
 
-              # APA formatted message.
-              apa = paste(round(proportion * 100, digits), "%, ", round(conf.level * 100, 0), "% CI [",
-                          round(conf.lower, digits), ", ", round(conf.upper, digits), "]", sep=""),
+                  # APA6 style format.
+                  apa = paste0(round(proportion * 100, digits), "%, ",
+                                     conf.level, "% CI [",
+                                     conf.lower, digits, ", ",
+                                     conf.upper, digits, "]"))
 
-              # Human-readable interpretation.
-              interpretation = paste("A share of ", round(proportion * 100, digits), "% with a sample size of ",
-                                     format(n, scientific = FALSE), " has a ", round(conf.level * 100, 0),
-                                     "% confidence interval between ", round(conf.lower, digits), " and ",
-                                     round(conf.upper, digits), " percentage points, and the margin of error is plus/minus ",
-                                     round(error, digits), " percentage points.", ifelse(population.correction,
-                                            paste0(" Note that these percentage points are corrected for the population size of ",
-                                                   format(population.size, scientific = FALSE), "."), ""), sep="")
-  ))
+  # Set S3 class.
+  class(moeobj) <- "moe"
+  return(moeobj)
+}
+
+
+# Check whether object is a "moe" object.
+is.moe <- function(obj) {
+  return(class(obj) == "moe")
+}
+
+
+# Print moe object with human-readable interpretation of the margin of error.
+print.moe <- function(obj, digits=NULL) {
+  if(!is.moe(obj)) {
+    stop("obj must be a 'moe' object.")
+  }
+  if(!is.null(digits)) {
+    cat(round(obj$margin.of.error, digits), "\n")
+  } else {
+    cat(obj$margin.of.error, "\n")
+  }
+}
+
+
+# Test if two proportions are significant using 2-sample test for equality of proportions (Pearson Chi-squared).
+`-.moe` <- function(obj1, obj2) {
+  if(!is.moe(obj1)) { stop("obj1 must be a 'moe' object.") }
+  if(!is.moe(obj2)) { stop("obj2 must be a 'moe' object.") }
+
+  # Use the confidence level from the first object.
+  conf.level <- obj1$conf.level
+  cat("Note: Using the ", conf.level, "% confidence level from '", deparse(substitute(obj1)), "'.\n", sep="")
+
+  proportion1 <- ceiling(obj1$proportion * obj1$n)
+  proportion2 <- ceiling(obj2$proportion * obj2$n)
+  n1 <- obj1$n
+  n2 <- obj2$n
+  test <- stats::prop.test(x = c(proportion1, proportion2),
+                           n = c(n1, n2),
+                           conf.level = conf.level / 100,
+                           alternative = "two.sided")
+  return(test)
+}
+
+
+# Inform user that this method is not allowed.
+`+.moe` <- function(obj1, obj2) {
+  stop(paste0("The minus operator is not meaningful for moe objects. Please use '", deparse(substitute(obj1)), " - ", deparse(substitute(obj2)), "' instead."))
+}
+
+
+# Indent text and wrap long lines.
+cat_wrap <- function(text, width=80, indent="") {
+  cat(indent, strwrap(text, width=width), sep=paste0("\n", indent))
+}
+
+
+# Print all available information from a moe object.
+summary.moe <- function(obj, digits=2, interpretation=TRUE) {
+  if(!is.moe(obj)) {
+    stop("obj must be a 'moe' object.")
+  }
+
+  # Human-readable interpretation.
+  interpretation.text = paste("A share of ", round(obj$percentage, digits), "% with a sample size of ",
+                              format(obj$n, scientific = FALSE), " has a ", round(obj$conf.level, 0),
+                              "% confidence interval between ", round(obj$conf.lower, digits), " and ",
+                              round(obj$conf.upper, digits), " percentage points, and the margin of error is plus/minus ",
+                              round(obj$margin.of.error, digits), " percentage points.",
+                              ifelse(obj$population.corrected,
+                                     paste0(" These percentage points are corrected for the population size of ",
+                                            format(obj$population.size, scientific = FALSE), "."), ""), sep="")
+
+  cat("Parameters\n\n")
+  cat(format("  Margin of error:", justify="left", width=32), obj$margin.of.error, "\n")
+  cat(format("  Proportion:", justify="left", width=32), obj$proportion, paste0("(", obj$percentage, "%)"), "\n")
+  cat(format("  Confidence level:", justify="left", width=32), paste0(obj$conf.level, "%"), "\n")
+  cat(format("  Confidence interval:", justify="left", width=32), paste0("[", round(obj$conf.lower, digits), ", ", round(obj$conf.upper, digits), "]"), "\n")
+  cat(format("  Sample size:", justify="left", width=32), paste0("n = ", obj$n), "\n")
+  cat(format("  z-value:", justify="left", width=32), obj$z, "\n")
+  cat(format("  APA6 style format:", justify="left", width=32), as.character(obj, digits=digits), "\n")
+  if(obj$population.corrected) {
+    cat("\nPopulation correction\n\n")
+    cat("  Note: Margin of error and confidence intervals are corrected for population size.\n\n")
+    cat(format("  Population size:", justify="left", width=32), paste0("N = ", format(obj$population.size, scientific = FALSE)), "\n")
+    cat(format("  Sampling fraction:", justify="left", width=32), obj$sampling.fraction, "\n")
+    cat(format("  Finite population correction:", justify="left", width=32), obj$fpc, "\n")
+    err.diff <- obj$error.uncorrected - obj$margin.of.error
+    cat(format("  Uncorrected margin of error:", justify="left", width=32), format(obj$error.uncorrected, scientific=FALSE), paste0("(difference: ", format(err.diff, scientific=FALSE), ")"), "\n")
+  }
+  if(interpretation) {
+    cat(format("\nInterpretation\n"))
+    cat_wrap(interpretation.text, indent="  ", width=80)
+  }
+}
+
+
+# Return character string with APA6 style formatting from moe object.
+as.character.moe <- function(obj, digits=2) {
+  if(!is.moe(obj)) {
+    stop("obj must be a 'moe' object.")
+  }
+  return(paste0(round(obj$proportion * 100, digits), "%, ",
+               obj$conf.level, "% CI [",
+               round(obj$conf.lower, digits), ", ",
+               round(obj$conf.upper, digits), "]"))
+}
+
+
+# Return margin of error as a double from moe object.
+as.double.moe <- function(obj) {
+  if(!is.moe(obj)) {
+    stop("obj must be a 'moe' object.")
+  }
+  return(obj$margin.of.error)
+}
+
+
+# Return margin of error as an integer from moe object.
+as.integer.moe <- function(obj) {
+  if(!is.moe(obj)) {
+    stop("obj must be a 'moe' object.")
+  }
+  return(as.integer(obj$margin.of.error))
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# moe: Calculate Margin of Error
+# moe
 
-R package **moe** calculates margin of error for simple probability samples.
+R package moe calculates margin of error for simple probability samples.
 
 ## Install
 
@@ -19,150 +19,152 @@ moe(proportion, n, conf.level = 0.95, digits = 2,
 
 ## Arguments
 
-- `proportion` ... value between 0 and 1 indicating the proportion, such as 0.30 for 30 percent.
-- `n` ... sample size.
-- `conf.level` ... confidence level (defaults to 0.95).
-- `digits` ... number of decimal digits used when formatting the results as APA and human-readable messages (defaults to 2).
-- `population.correction` ... whether or not results should be corrected by population size (defaults to FALSE).
-- `population.size` ... population size used by the population correction (defaults to NULL). Only used if population.correction is set to TRUE.
+- `proportion` = value between 0 and 1 indicating the proportion, such as 0.30 for 30 percent.
+- `n` = sample size.
+- `conf.level` = confidence level (defaults to 0.95).
+- `digits` = number of decimal digits used when formatting the results as APA and human-readable messages (defaults to 2).
+- `population.correction` = whether or not results should be corrected by population size (defaults to FALSE).
+- `population.size` = population size used by the population correction (defaults to NULL). Only used if population.correction is set to TRUE.
 
 ## Returns
 
-a list with margin of error, confidence level, confidence interval (lower and upper bound), z-value, results formatted in accordance to APA standards, and human-readable interpretation of results.
+a list with:
+
+- `margin.of.error` = margin of error (in percentage points).
+- `conf.level` = confidence level (in percentage points).
+- `conf.lower` = confidence interval lower bound (in percentage points).
+- `conf.upper` = confidence interval upper bound (in percentage points).
+- `proportion` = proportion (same as input parameter).
+- `percentage` = proportion expressed as percentage.
+- `z.value` = z-value from normal distribution.
+- `digits` = number of digits used to format APA confidence intervals.
+- `n` = sample size (same as input parameter).
+- `population.corrected` = whether or not the margin of error is corrected for population size (same as input parameter).
+- `population.size` = population size (same as input parameter).
+- `fpc` = finite population correction, between 0 and 1.
+- `sampling.fraction` = sampling fraction, ratio of sample size to population size, between 0 and 1.
+- `error.uncorrected` = margin of error before it is corrected for population size (in percentage points).
+- `apa` = APA6 style formatted confidence intervals, such as 43.2%, 95% CI [40.1, 46.5].
 
 ## Examples
 
 In this case, a political party got 30% in a sample of 1,200 voters.
 
 ```r
+# Get margin of error.
 moe(proportion=0.30, n=1200)
 ```
 
 Which outputs:
 
 ```r
-$`margin.of.error`
 [1] 2.592789
-
-$conf.level
-[1] 95
-
-$conf.lower
-[1] 27.40721
-
-$conf.upper
-[1] 32.59279
-
-$proportion
-[1] 0.3
-
-$percentage
-[1] 30
-
-$z.value
-[1] 1.959964
-
-$digits
-[1] 2
-
-$n
-[1] 1200
-
-$population.corrected
-[1] FALSE
-
-$population.size
-NULL
-
-$apa
-[1] "30%, 95% CI [27.41, 32.59]"
-
-$interpretation
-[1] "A share of 30% and a sample size of 1200 has a 95% confidence interval between 27.41 and 32.59 percentage points, and the margin of error is plus/minus 2.59 percentage points."
 ```
 
-As you can see above, the output includes (among other things) margin of error, confidence intervals, results formatted in accordance with APA6 standards, as well as human-readable interpretation.
+Common methods such as `summary()` and `print()` are supported, as well as  `as.character()`, `as.double()`, and `as.integer()`.
 
-You can also extract specific data:
+With `summary()`, all available information is given:
 
 ```r
-m <- moe(proportion=0.3, n=1200)
-m$margin.of.error
+m <- moe(proportion=0.30, n=1200)
+summary(m, digits=2)
+```
+
+Which outputs:
+
+```
+Parameters
+
+  Margin of error:               2.592789 
+  Proportion:                    0.3 (30%) 
+  Confidence level:              95% 
+  Confidence interval:           [27.41, 32.59] 
+  Sample size:                   n = 1200 
+  z-value:                       1.959964 
+  APA6 style format:             30%, 95% CI [27.41, 32.59] 
+
+Interpretation
+  
+  A share of 30% with a sample size of 1200 has a 95% confidence interval between
+  27.41 and 32.59 percentage points, and the margin of error is plus/minus 2.59
+  percentage points.
+```
+
+Using the `as.character()` method, you can extract the APA6 styled confidence intervals:
+
+```r
+# Get APA6 confidence intervals.
+m <- moe(proportion=0.30, n=1200)
+as.character(m, digits=2)
 ```
 
 Which outputs:
 
 ```r
-[1] 2.592789
+[1] "30%, 95% CI [27.41, 32.59]"
 ```
 
-You can also correct the results for population size. In essence, the closer the sample size is to the population size, the smaller the margin of error will be. In everyday survey research (where n = 1,000), however, the effect of population correction is trivial.
+You can also extract specific data from the returned list:
+
+```r
+m <- moe(proportion=0.3, n=1200)
+m$conf.lower
+m$conf.upper
+```
+
+Which outputs:
+
+```r
+[1] 27.40721
+[1] 32.59279
+```
+
+## Correct margin of error for large samples
+
+If your sample is large, you can correct for population size using [finite population correction](https://en.wikipedia.org/wiki/Standard_error#Correction_for_finite_population). In essence, the closer the sample size is to the population size, the smaller the margin of error will be. In everyday survey research (where typical n = 1,000 and population size is millions), however, the effect of population correction is trivial.
 
 In this example, the sample is 50,000. We correct for population size (in this fictional country with 300,000 voters) and increase the confidence level to 99%.
 
 ```r
-moe(proportion=0.355, n=50000, conf.level=0.99, population.correction=TRUE, population.size=300000)
+m <- moe(proportion=0.355, n=50000, conf.level=0.99, population.correction=TRUE, population.size=300000)
+summary(m)
 ```
 
 Which outputs:
 
-```r
-$`margin.of.error`
-[1] 0.4593512
+```
+Parameters
 
-$conf.level
-[1] 99
+  Margin of error:               0.4593512 
+  Proportion:                    0.355 (35.5%) 
+  Confidence level:              99% 
+  Confidence interval:           [35.04, 35.96] 
+  Sample size:                   n = 50000 
+  z-value:                       2.575829 
+  APA6 style format:             35.5%, 99% CI [35.04, 35.96] 
 
-$conf.lower
-[1] 35.04065
+Population correction
 
-$conf.upper
-[1] 35.95935
+  Note: Margin of error and confidence intervals are corrected for population size.
 
-$proportion
-[1] 0.355
+  Population size:               N = 300000 
+  Sampling fraction:             0.1666667 
+  Finite population correction:  0.8333333 
+  Uncorrected margin of error:   0.5512215 (difference: 0.09187024) 
 
-$percentage
-[1] 35.5
-
-$z.value
-[1] 2.575829
-
-$digits
-[1] 2
-
-$n
-[1] 50000
-
-$population.corrected
-[1] TRUE
-
-$population.size
-[1] 3e+05
-
-$apa
-[1] "35.5%, 99% CI [35.04, 35.96]"
-
-$interpretation
-[1] "A share of 35.5% with a sample size of 50000 has a 99% confidence interval between 35.04 and 35.96 percentage points, and the margin of error is plus/minus 0.46 percentage points. Note that these percentage points are corrected for the population size of 300000."
+Interpretation
+  
+  A share of 35.5% with a sample size of 50000 has a 99% confidence interval
+  between 35.04 and 35.96 percentage points, and the margin of error is
+  plus/minus 0.46 percentage points. These percentage points are corrected for
+  the population size of 300000.
 ```
 
-You can also compare margin of errors with and without population correction:
+We also see that the margin of error before correction is 0.5512215 and after correction it is 0.4593512. These are percentage points, so the the difference (0.09187024) is not particularly large in this case.
 
-```
-moe(proportion=0.355, n=50000, conf.level=0.99, population.correction=TRUE, population.size=300000)$margin.of.error
-moe(proportion=0.355, n=50000, conf.level=0.99)$margin.of.error
-```
+The [sampling fraction](https://en.wikipedia.org/wiki/Sampling_fraction) is the ratio of sample size to population size, which is about 0.16. The closer the sampling fraction is to 1, the closer the sample size is to the population size.
 
-Which outputs:
+## History
 
-```r
-[1] 0.4593512
-[1] 0.5512215
-```
-
-As you can see above, the corrected method (first line) give somewhat smaller margin of error, in this particular case.
-
-# History
-
+- 2018-08-20 Version 0.9.1 Uses S3 class and common generic methods (e.g., `summary`).
 - 2018-08-14 Version 0.9.0 First release.
